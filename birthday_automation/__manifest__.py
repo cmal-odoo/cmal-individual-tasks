2a76e141e6d53587c4c411222798f327f1d2eb84
cmal-odoo <cmal@odoo.com>
2023-06-29 14:14:26 -0400

    Initial code commit

---
+++ birthday_automation/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    "name": "Birthday Automation",
+    
+    "summary": "Birthday Automation individual task",
+    
+    "description": """
+    Birthday Automation
+====================
+This Module is used to automate employee birthday emails
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Individual/Tasks",
+    
+    "license": "OPL-1",
+    
+    "depends": ["hr", "mail"],
+    
+    "data": [
+        'data/email_template.xml',
+        'data/ir_cron_data.xml'
+    ],
+    
+    "demo": [],
+    
+    "author": "cmal",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
